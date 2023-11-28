8e7b1d2a2cd4d0fc9694a5743f195538fe8e381c
GIL32610 <gil.00000032610@unicap.br>
2023-11-28 18:40:38 -0300

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent{
+            DiceRollerTheme {
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+
+
+
+@Composable
+fun DiceWithButtonAndImage(
+    modifier: Modifier = Modifier
+){
+    Column(modifier = modifier,
+        horizontalAlignment= Alignment.CenterHorizontally,
+
+    ){
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DiceRollerApp(){
+    DiceWithButtonAndImage(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center))
+}
